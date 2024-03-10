f5bd738f794fd18ea17fd7b1d562cef5dd595566
Shafayet <11shafayet@gmail.com>
2024-03-10 16:29:41 +0600

    for prod

--- app/page.js
+++ app/page.js
@@ -1,113 +1,153 @@
-import Image from "next/image";
+'use client';
 
-export default function Home() {
+import { useEffect, useState } from 'react';
+
+const Home = () => {
+  const [fromDate, setFromDate] = useState('');
+  const [fromError, setFromError] = useState(false);
+  const [toDate, setToDate] = useState('');
+  const [toError, setToError] = useState(false);
+
+  // set the dates of today on page render
+  useEffect(() => {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+    const formattedDate = `${year}-${month}-${day}`;
+    setFromDate(formattedDate);
+    setToDate(formattedDate);
+  }, []);
+
+  // 'from' date change handle
+  const handleFromChange = (e) => {
+    const selectedDate = new Date(e.target.value);
+    const today = new Date();
+    const yesterday = new Date(today);
+    const tomorrow = new Date(today);
+
+    yesterday.setDate(today.getDate() - 1);
+    tomorrow.setDate(today.getDate() + 1);
+
+    setFromDate(e.target.value);
+
+    if (
+      selectedDate.toISOString().slice(0, 10) !==
+        today.toISOString().slice(0, 10) &&
+      selectedDate.toISOString().slice(0, 10) !==
+        yesterday.toISOString().slice(0, 10) &&
+      selectedDate.toISOString().slice(0, 10) !==
+        tomorrow.toISOString().slice(0, 10)
+    ) {
+      setFromError(true);
+    } else {
+      setFromError(false);
+    }
+  };
+
+  // 'to' date change handle
+  const handleToChange = (e) => {
+    const selectedDate = new Date(e.target.value);
+    const fromDateObj = new Date(fromDate);
+    const tomorrow = new Date(fromDateObj);
+    tomorrow.setDate(fromDateObj.getDate() + 1);
+    const threeDaysLater = new Date(fromDateObj);
+    threeDaysLater.setDate(fromDateObj.getDate() + 3);
+
+    setToDate(e.target.value);
+
+    if (
+      selectedDate.toISOString().slice(0, 10) !==
+        tomorrow.toISOString().slice(0, 10) &&
+      (selectedDate < fromDateObj || selectedDate > threeDaysLater)
+    ) {
+      setToError(true);
+    } else {
+      setToError(false);
+    }
+  };
+
+  // handleSubmit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (fromError || toError) {
+      alert('Please fill up the fields by fullfiling the conditions!');
+    } else {
+      console.log(fromDate, toDate);
+      alert('Form sent Successfully!');
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <section className="py-12 md:py-20 relative">
+      <div className="container mx-auto px-4">
+        <div>
+          <h1 className="text-center font-bold text-3xl">UPdOC</h1>
+          <h2 className="text-center font-bold text-lg text-[#211252] my-3">
+            How long do you need this for?
+          </h2>
+
+          <p className="text-center font-medium text-sm opacity-75 mb-6">
+            If suitable, your Partner Doctor might change the end date based on
+            what they believe is appropriate.
+          </p>
+
+          {/* form */}
+          <form onSubmit={handleSubmit} className="max-w-xl mx-auto">
+            {/* from */}
+            <div className="flex flex-col gap-1">
+              <label htmlFor="from" className="font-medium">
+                From
+              </label>
+              <input
+                type="date"
+                className="w-full p-4 rounded-md text-base border"
+                value={fromDate}
+                onChange={handleFromChange}
+              />
+              {fromError && (
+                <p className="text-red-500 text-[12px] uppercase px-2 pt-1">
+                  PLEASE SELECT A DATE THAT IS YESTERDAY, TODAY, OR TOMORROW.
+                </p>
+              )}
+            </div>
+
+            {/* to */}
+            <div className="flex flex-col gap-1 mt-4">
+              <label htmlFor="toDate" className="font-medium">
+                To
+              </label>
+              <input
+                type="date"
+                id="toDate"
+                className="w-full p-4 rounded-md text-base border"
+                value={toDate}
+                onChange={handleToChange}
+              />
+              {toError && (
+                <p className="text-red-500 text-[12px] uppercase px-2 pt-1">
+                  IMPORTANT: The amount of days you've selected exceeds the
+                  average number of days Partner Doctors will typically provide
+                  a consultation for. Your Doctor may, at their discretion,
+                  request more information or determine a different outcome to
+                  ensure proper care.
+                </p>
+              )}
+            </div>
+
+            {/* submit button */}
+            <button
+              type="submit"
+              className="bg-[#684490] text-white mt-6 py-3 w-full rounded-md"
+            >
+              Continue
+            </button>
+          </form>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </section>
   );
-}
+};
+
+export default Home;
